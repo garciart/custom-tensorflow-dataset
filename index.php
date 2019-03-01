20015e836eefaee8cbb23111ec1adaa4ecef809f
garciart <rgarcia@rgprogramming.com>
2019-03-01 00:22:21 -0500

    Add primitive stop word removal capability.

--- index.php
+++ index.php
@@ -9,19 +9,26 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Blue Harvest</title>
         <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@0.11.2"></script>
+        <script type="text/javascript" src="cleantext.js"></script>
         <script type="text/javascript" src="tensorflow.js"></script>
     </head>
     <body>
         <?php
         ?>
-        <input type="text" name="enter" class="enter" value="" id="yValue"/>
-        <input type="button" value="click" onclick="yInput();"/>
-        <script type="text/javascript">
-            var lol = document.getElementById('yValue');
-            function yInput() {
-                learnLinear(yValue.value);
-            }
-        </script>
+        <p>Enter text below:</p>
+        <textarea id="myText" rows="8" cols="80">
+At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies. 
+        </textarea>
+        <br>
+        <input type="button" value="Remove Stop Words" onclick="hello();"/>
+        <p>Enter an x-value: <input type="text" name="enter" class="enter" value="" id="yValue"/>
+            <input type="button" value="Get y-Value" onclick="yInput();"/>
+            <script type="text/javascript">
+                var lol = document.getElementById('yValue');
+                function yInput() {
+                    learnLinear(yValue.value);
+                }
+            </script></p>
         <div id="output_field"></div>
     </body>
 </html>
