e984178b23fafc03c30c3f7e900023091419c255
garciart <rgarcia@rgprogramming.com>
2019-03-02 19:08:15 -0500

    Tweeked text prep and added feature scaling. Added a Master Page.

---
+++ Master.php
@@ -0,0 +1,37 @@
+<?php
+/* Source: https://github.com/garciart/Master-Pages-In-PHP */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Blue Harvest</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@0.11.2"></script>
+        <script type="text/javascript" src="textPrep.js"></script>
+        <script type="text/javascript" src="tensorflow.js"></script>
+        <!-- Content placeholder for head content -->
+        <?php echo $contentPlaceHolder1; ?>
+    </head>
+    <body>
+        <header>
+            <div style="clear:both; overflow:auto;">
+                <img src="sherpa_q2a_logo.png" alt="" style="float:left; height:100px;"/>
+                <h1>Welcome to Blue Harvest #1!</h1>                
+
+                <!-- Content placeholder for header element content -->
+                <?php echo $contentPlaceHolder2; ?>
+                <hr>
+            </div>
+        </header>
+        <main>
+            <h2>And here we go...</h2>
+            <!-- Content placeholder for main element content -->
+            <?php echo $contentPlaceHolder3; ?>
+            <hr>
+        </main>
+        <footer>
+            <!-- Content placeholder for footer element content -->
+            <?php echo $contentPlaceHolder4; ?>
+        </footer>
+    </body>
+</html>
--- textPrep.js
+++ textPrep.js
@@ -9,22 +9,23 @@ function textPrepController() {
     var context = document.getElementById('myText').value;
     var contextArray = removeStopWords(context);
     var frequencyMap = getFrequency(contextArray);
-    var sortedArray = sortMap(frequencyMap);
-    var results = "";
-    for(var i = 0; i < sortedArray.length; i++) {
-        if(i == 0) {
-            alert("Here too!");
-        }
-        results += "\n" + sortedArray[i].name + "\t" + sortedArray[i].total;
+    var sortedArray = convertToSortedArray(frequencyMap);
+    var results = "Breakdown:";
+    var arrayMax = sortedArray[0][1];
+    // Setting minimum to zero allows all x's to be weighted
+    var arrayMin = 0;
+    for (var i = 0; i < sortedArray.length; i++) {
+        var featureScaled = (sortedArray[i][1] - arrayMin) / (arrayMax - arrayMin);
+        results += "\n" + sortedArray[i][0] + "\t" + featureScaled;
+        // results += "\n" + sortedArray[i].name + "\t" + sortedArray[i].count;
     }
-    alert("Here!");
     document.getElementById('myText').value = results;
 
 }
 
 function removeStopWords(context) {
     // Load stop words. Source: https://www.nltk.org/nltk_data/
-    var stopWords = ["a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn", "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't", "having", "he", "her", "here", "hers", "herself", "him", "himself", "his", "how", "i", "if", "in", "into", "is", "isn", "isn't", "it", "its", "it's", "itself", "just", "ll", "m", "ma", "me", "mightn", "mightn't", "more", "most", "mustn", "mustn't", "my", "myself", "needn", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "our", "ours", "ourselves", "out", "over", "own", "re", "s", "same", "shan", "shan't", "she", "she's", "should", "shouldn", "shouldn't", "should've", "so", "some", "such", "t", "than", "that", "that'll", "the", "their", "theirs", "them", "themselves", "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we", "were", "weren", "weren't", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "won", "won't", "wouldn", "wouldn't", "y", "you", "you'd", "you'll", "your", "you're", "yours", "yourself", "yourselves", "you've"];
+    var stopWords = ["a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn", "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during", "each", "e.g.", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't", "having", "he", "her", "here", "hers", "herself", "him", "himself", "his", "how", "i.e.", "i", "if", "in", "into", "is", "isn", "isn't", "it", "its", "it's", "itself", "just", "ll", "m", "ma", "me", "mightn", "mightn't", "more", "most", "mustn", "mustn't", "my", "myself", "needn", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "our", "ours", "ourselves", "out", "over", "own", "re", "s", "same", "shan", "shan't", "she", "she's", "should", "shouldn", "shouldn't", "should've", "so", "some", "such", "t", "than", "that", "that'll", "the", "their", "theirs", "them", "themselves", "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we", "were", "weren", "weren't", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "won", "won't", "wouldn", "wouldn't", "y", "you", "you'd", "you'll", "your", "you're", "yours", "yourself", "yourselves", "you've"];
     // Convert the text to lowercase and replace all special characters with spaces
     context = context.toLowerCase().replace(/[^\w\s+]/gi, ' ');
     // Loop text, replacing stopwords with spaces duting each iteration
@@ -51,17 +52,32 @@ function getFrequency(contextArray) {
     return frequencyMap;
 }
 
-function sortMap(frequencyMap) {
+function convertToSortedArray(frequencyMap) {
     // sort by count in descending order
     var sortedArray = [];
-    sortedArray = Object.keys(frequencyMap).map(function (key) {
-        return {
-            name: key,
-            total: frequencyMap[key]
-        };
-    });
+    for (var key in frequencyMap) {
+        sortedArray.push([key, frequencyMap[key]]);
+    }
+
     sortedArray.sort(function (a, b) {
-        return b.total - a.total;
+        // Sort by frequency in descending order. Change to a[0] and b[0] to sort by name
+        if (a[1] === b[1]) {
+            return 0;
+        } else {
+            return (a[1] > b[1]) ? -1 : 1;
+        }
     });
+    /*
+     sortedArray = Object.keys(frequencyMap).map(function (key) {
+     return {
+     name: key,
+     count: frequencyMap[key]
+     };
+     });
+     sortedArray.sort(function (a, b) {
+     // return (a.count > b.count) ? -1 : ((a.count < b.count) ? 1 : 0);
+     return b.name - a.name;
+     });
+     */
     return sortedArray;
 }
